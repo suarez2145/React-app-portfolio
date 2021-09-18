a7d89361890c4ab17e3478793e0032788d0eb066
suarez2145 <suarez.exxon@gmail.com>
2021-09-18 14:10:27 -0700

    removed excess commented notes

--- src/App.js
+++ src/App.js
@@ -15,21 +15,6 @@ function App() {
       <ToolsSection />
       <AboutMe />
       <Projects />
-
-      
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      
     </div>
   );
 }
