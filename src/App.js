dc2039fcb13f8b88568d2223e8e3c7ae1313b5fe
suarez2145 <suarez.exxon@gmail.com>
2023-01-10 03:39:10 -0800

    added engines section to package json file

--- src/App.js
+++ src/App.js
@@ -5,28 +5,42 @@ import NavBar from './components/NavBar';
 import ToolsSection from './components/ToolsSection';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
+// import LoaderPage from './components/LoaderPage';
+// import { useState, useEffect } from 'react';
+
 
 
 
 function App() {
 
-  
+  // code below is if i want to test load spinner by adding a state and a setTimeout delay to the rendering of the components
+  // const [count, setCount] = useState(false)
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setCount(true), 1000);
+  //   console.log('the delay worked???1')
+  //   return () => clearTimeout(timer);
+  // });
+
 
   return (
 
-
-    
-
     <div className="App">
+      {/*  code below is for conditionally rendering the components if state is true and if false then load loader page which renders spinner */}
+      {/* {count ? <NavBar />  : <LoaderPage />}
+      {count ? <Header /> : null}
+      {count ? <AboutMe /> : null}
+      {count ? <ToolsSection /> : null}
+      {count ? <Projects /> : null} */}
+
+
       <NavBar />
-      <Header />
-      <AboutMe />
+      <Header /> 
+      <AboutMe /> 
       <ToolsSection />
       <Projects />
 
     </div>
-
-    
 
   );
 
---
+++ src/components/LoaderPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const LoaderPage = () => {
+
+
+    return (
+        <div className="divLoader">
+            <img className="spin_class_load" src="images/loading-96.png" alt="...Loading"></img>
+        </div>
+    )
+}
+
+
+export default LoaderPage;
